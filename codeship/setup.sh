e523b0b1cf0836207a4c6b196fb8e502c706b74d
Chris Lee <sihrc.c.lee@gmail.com>
2015-09-13 02:35:33 -0400

    CLEAN: clean app.js / gulpfile.js

--- codeship/setup.sh
+++ codeship/setup.sh
@@ -1,4 +1,8 @@
 # By default we use the Node.js version set in your package.json or 0.10.25
 # You can use nvm to install any Node.js version.
 # i.e.: nvm install 0.10.25
-npm install --dev
+# npm cache clean -f
+# npm install npm -g
+# npm install -g n
+rm -rf node_modules
+npm install
