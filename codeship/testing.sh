684cb86ddbb6c61306761983ea362d94f1a2162b
David Zhu <hdavidzhu@Davids-MacBook-Air-7.local>
2015-09-25 11:39:10 -0400

    ADD: Coveralls support.
    UPDATE: package.json with coveralls.
    
    ADD: coveralls credentials via environment variable.
    
    REMOVE: turns out coveralls.yml is not required.

--- codeship/testing.sh
+++ codeship/testing.sh
@@ -1,1 +1,4 @@
-./node_modules/.bin/istanbul cover ./node_modules/.bin/_mocha tests.js
+# ./node_modules/.bin/istanbul cover ./node_modules/.bin/_mocha tests.js
+
+# This is now running Coveralls.
+./node_modules/.bin/istanbul cover ./node_modules/.bin/_mocha tests.js --report lcovonly -- -R spec && cat ./coverage/lcov.info | ./node_modules/coveralls/bin/coveralls.js && rm -rf ./coverage
