ff7f1d6ffdabb0fd551a956c5ac6131db15ff255
Chris Lee <sihrc.c.lee@gmail.com>
2015-09-26 13:19:53 -0400

    ADD: V2 chnages

--- src/jsx/profile/profile.jsx
+++ src/jsx/profile/profile.jsx
@@ -81,22 +81,21 @@ var Profile = React.createClass({
       <div className="form-container">
         <div className="form">
           <h1>
-            hello! nice to meet you</h1>
-          <div>we are a group of [engineers] looking to house together.  we enjoy [working on projects together], and we are looking for people to join us!</div>
-          <p className="title">we would love to learn a little more about you</p>
+            Hello! Nice to meet you</h1>
+          <div>We are a group of [engineers] looking to house together.  We enjoy [working on projects together], and we are looking for people to join us!</div>
+          <p className="title">We would love to learn a little more about you.</p>
           <input id="name" placeholder="Name" type="text"></input>
           <input id="email" placeholder="Email" type="text"></input>
           <input id="gender" placeholder="Gender" type="text"></input>
           <hr></hr>
-          <p className="title">around how old are you</p>
+          <p className="title">Around how old are you?</p>
           <div className="triple-form">
             <div className="age-box">20</div>
             <div className="age-box">30</div>
             <div className="age-box">40</div>
           </div>
           <hr></hr>
-          <p className="title">when are you usually asleep</p>
-          <div className="label">PM</div>
+          <p className="title">When are you usually asleep?</p>
           <div className="timeline">
             <div className="no-border time label">NOON</div>
             {Array.apply(1, Array(11)).map(function (v, i) {
@@ -105,10 +104,6 @@ var Profile = React.createClass({
               )
             })}
             <div className="time label">MIDNIGHT</div>
-          </div>
-          <div className="label">AM</div>
-          <div className="timeline">
-            <div className="no-border time label">MIDNIGHT</div>
             {Array.apply(1, Array(11)).map(function (v, i) {
               return (
                 <div className="time am">{i + 1}</div>
@@ -117,33 +112,33 @@ var Profile = React.createClass({
             <div className="time label">NOON</div>
           </div>
           <hr></hr>
-          <p className="title">how clean do you like it</p>
+          <p className="title">How clean do you like it?</p>
           <div className="triple-form">
             <img id="clean" className='clean-box' src={"images/clean.png"}></img>
             <img id="medium" className='clean-box' src={"images/medium.png"}></img>
             <img id="messy" className='clean-box' src={"images/messy.png"}></img>
           </div>
           <hr></hr>
-          <p className="title">your comfortable noise levels</p>
+          <p className="title"> What is your comfortable noise level?</p>
           <div className="triple-form">
             <img id="library" className='noise-box' src={"images/library.png"}></img>
             <img id="cafe" className='noise-box' src={"images/cafe.png"}></img>
             <img id="party" className='noise-box' src={"images/party.png"}></img>
           </div>
           <hr></hr>
-          <p className="title">preferred amount of interaction with house mates</p>
+          <p className="title">What is your preferred amount of interaction with house mates?</p>
           <div className="triple-form">
             <div className="interaction-box">Like Strangers</div>
             <div className="interaction-box">Hangout Sometimes</div>
             <div className="interaction-box">Everything together</div>
           </div>
           <hr></hr>
-          <p className="title">what are you interested in, why house with us</p>
+          <p className="title">What are you interested in? Why house with us?</p>
           <textarea id="interests"></textarea>
           <hr></hr>
           <div className="triple-form align-left">
-            <p className="title">thanks for taking the time, talk to you soon!</p>
-            <div className="submit spaced" onClick={this.submit}>done</div>
+            <p className="title">Thanks for taking the time. Talk to you soon!</p>
+            <div className="submit spaced" onClick={this.submit}>DONE</div>
           </div>
         </div>
       </div>
