dd473be6e41861e90036de727fcfe0bcce1c9922
David Zhu <hdavidzhu@gmail.com>
2015-10-31 21:15:56 -0400

    CANCELLED: Written tests for this branch.

--- src/jsx/chat_box/chat_box_test.jsx
+++ src/jsx/chat_box/chat_box_test.jsx
@@ -11,13 +11,13 @@ var ChatBox = require("./chat_box.jsx");
 // var message_html = "[10] mocked: mocked text";
 
 module.exports = function(render, find, utils) {
-  beforeEach("Render and locate element", function(done) {
-    var rendered = render( <ChatBox /> );
-    this.div = find(rendered, "div");
-    done();
-  });
-
-  it('<div> should have className -> messages', function() {
-    expect(this.div.getAttribute('class')).to.be.equal("message");
-  });
+  // beforeEach("Render and locate element", function(done) {
+  //   var rendered = render( <ChatBox /> );
+  //   this.div = find(rendered, "div");
+  //   done();
+  // });
+  //
+  // it('<div> should have className -> messages', function() {
+  //   expect(this.div.getAttribute('class')).to.be.equal("message");
+  // });
 }
